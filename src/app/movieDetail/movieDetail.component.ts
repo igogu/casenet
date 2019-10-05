b29f792947667f62160488758edd120ec1aed8ff
Iván Gómez Gutiérrez <ivangomezgutierrez87@gmail.com>
2019-10-05 13:32:51 +0200

     Display a list of names of all characters that appear in the movie (you can get the characters

--- src/app/movieDetail/movieDetail.component.ts
+++ src/app/movieDetail/movieDetail.component.ts
@@ -1,7 +1,8 @@
+import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { forkJoin, Observable } from 'rxjs';
 @Component({
   selector: 'casenet-movie-detail',
   templateUrl: './movieDetail.component.html',
@@ -13,15 +14,33 @@ export class MovieDetailComponent implements OnInit{
   url = 'https://swapi.co/api/films/';
   movieId = 0;
   movieDetail: any;
-  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  characters = [];
+
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private location: Location ) {}
 
   ngOnInit() {
     this.movieId = this.route.snapshot.params.id;
     this.url += this.movieId;
     this.http.get(this.url).subscribe((movie: any) => {
       this.movieDetail = movie;
-      console.log(movie);
+      this.characterInfo(this.movieDetail.characters).subscribe((data: any) => {
+          data.forEach(elem => {
+            this.characters.push(elem);
+          });
+        });
     });
   }
 
+  characterInfo(characters: any): Observable<any[]> {
+    const observableBatch = [];
+    for (const char of characters) {
+      observableBatch.push( this.http.get( char ));
+    }
+    return forkJoin(observableBatch);
+  }
+
+  back() {
+    this.location.back();
+  }
 }
