0730196b2468af067d9dc9ec9bea5efcc383a536
Iván Gómez Gutiérrez <ivangomezgutierrez87@gmail.com>
2019-10-05 11:03:45 +0200

    The table with results supports pagination. Page size is 5 items.

--- src/app/movieList/movieList.component.ts
+++ src/app/movieList/movieList.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'casenet-movie-list',
@@ -10,12 +10,45 @@ export class MovieListComponent implements OnInit{
 
   title = 'Star Wars Titles';
   url = 'https://swapi.co/api/films/';
-  films: any[]
+  films: any[];
+  filmsInPage: any[];
+  pageSize = 5;
+  currentPage = 1;
+  lastPage = 1;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get(this.url).subscribe((films: any) => {
       this.films = films.results;
-      console.log(this.films)});
+      this.updatePageFilms();
+      this.updateLastPage();
+    });
+  }
+
+  nextPage() {
+    if (this.currentPage < this.lastPage) {
+      this.currentPage++;
+      this.updatePageFilms();
+    }
+  }
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePageFilms();
+    }
+  }
+  endPage() {
+    this.currentPage = this.lastPage;
+    this.updatePageFilms();
+  }
+  firstPage() {
+    this.currentPage = 1;
+    this.updatePageFilms();
+  }
+  updatePageFilms() {
+    this.filmsInPage = this.films.slice((this.currentPage-1) * this.pageSize, (this.currentPage-1) * this.pageSize + this.pageSize);
+  }
+  updateLastPage() {
+    this.lastPage = Math.ceil(this.films.length / this.pageSize);
   }
 }
